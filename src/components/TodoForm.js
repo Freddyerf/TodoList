8e62c23a38cb2af7e320a745e4d45ce6596e9838
Freddy Rondon <frondonfermin@gmail.com>
2020-10-13 00:45:15 -0400

    Form component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,9 @@
+import {Button, TextField} from "@material-ui/core";
 import React, {useState} from "react";
+import {v4 as uuid} from "uuid";
 
 
-function TodoForm(){
+function TodoForm({addTodo}){
     const [todo, setTodo] = useState({
         id: "",
         task: "",
@@ -12,15 +14,25 @@ function TodoForm(){
         setTodo({...todo, task: e.target.value});
     }
 
+    function handlesSubmit(e) {
+        e.preventDefault();
+        if (todo.task.trim()) {
+            addTodo({...todo, id: uuid()});
+            // reset task input
+            setTodo({...todo, task: ""});
+        }
+    }
+
     return (
-        <form>
-            <input
+        <form className="todo-form" onSubmit={handlesSubmit}>
+            <TextField
+                label="Task"
                 name="taks"
                 type="text"
                 value={todo.task}
                 onChange={handleTaskInputChange}
             />
-            <button type="submit" />
+            <Button type="submit">submit</Button>
         </form>
     );
 }
